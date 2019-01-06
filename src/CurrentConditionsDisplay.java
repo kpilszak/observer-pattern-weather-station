4d073f0b5bcabbb02da2bc3d7f86341a0b08fb35
kpilszak <klaudia.pilszak@gmail.com>
2019-01-06 23:03:08 +0100

    Use constructor in CurrentConditionsDisplay to register a new observer

--- src/CurrentConditionsDisplay.java
+++ src/CurrentConditionsDisplay.java
@@ -1,3 +1,10 @@
 public class CurrentConditionsDisplay implements Observer {
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
 
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
 }
