00d97569c8aedc2297368d5f281e42aa8ae96ae4
kpilszak <klaudia.pilszak@gmail.com>
2019-01-06 23:07:44 +0100

    Create observer and simulate new weather measurements

--- src/WeatherStation.java
+++ src/WeatherStation.java
@@ -1,5 +1,7 @@
 public class WeatherStation {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        weatherData.setMeasurements(80, 65);
     }
 }
