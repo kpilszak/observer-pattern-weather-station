a1e100d77931749437c35be1e4bec5c37445bbf4
kpilszak <klaudia.pilszak@gmail.com>
2019-01-06 22:52:10 +0100

    Add variables in WeatherData

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -1,2 +1,9 @@
-public class WeatherData {
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+    private ArrayList observers;
+    private float temperature;
+    private float humidity;
+
+    
 }
