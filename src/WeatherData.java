715c0b36174374b2b382081e59f10fd92f8c2058
kpilszak <klaudia.pilszak@gmail.com>
2019-01-06 23:00:20 +0100

    Add methods registering and notifying observers

--- src/WeatherData.java
+++ src/WeatherData.java
@@ -8,4 +8,31 @@ public class WeatherData implements Subject {
     public WeatherData() {
         observers = new ArrayList();
     }
+
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    public void removeObserver(Observer o) {
+        int i = observers.indexOf(o);
+        if (i >= 0)
+            observers.remove(i);
+    }
+
+    public void notifyObservers() {
+        for (int i = 0; i < observers.size(); i++) {
+            Observer observer = (Observer)observers.get(i);
+            observer.update(temperature, humidity);
+        }
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+
+    public void setMeasurements(float temperature, float humidity) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        measurementsChanged();
+    }
 }
