dde6d68cdfee81bb308acb310827678049c8b060
kpilszak <klaudia.pilszak@gmail.com>
2019-01-06 23:04:48 +0100

    Add update methods in observer class

--- src/CurrentConditionsDisplay.java
+++ src/CurrentConditionsDisplay.java
@@ -7,4 +7,14 @@ public class CurrentConditionsDisplay implements Observer {
         this.weatherData = weatherData;
         weatherData.registerObserver(this);
     }
+
+    public void update(float temperature, float humidity) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
 }
